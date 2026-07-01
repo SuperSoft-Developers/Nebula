b9aeda2fab0a2f6912d11f23cae05e469f9f70e1
SuperSoft-Developers <begameguidi@gmail.com>
2026-07-01 16:39:23 -0300

    Update Game

--- core/renderer.h
+++ core/renderer.h
@@ -1,16 +1,23 @@
 #pragma once
 
+#include "tilemap.h"
+#include "renderer.h"
+#include "renderer.h"
+
 typedef struct {
     int x;
     int y;
     int w;
     int h;
 
-    const unsigned short* pixels; // formato 3DS (RGB565)
+    const unsigned short* pixels;
 } NebulaSprite;
 
+/* sprites */
 void renderer_init(int width, int height);
 void renderer_clear();
-
-void nebula_draw_sprite(NebulaSprite* sprite, int x, int y);
 void renderer_present();
+void nebula_draw_sprite(NebulaSprite* sprite, int x, int y);
+
+/* 💥 ADICIONE ISSO */
+void nebula_draw_tilemap(NebulaTilemap* map, NebulaSprite* tileSprite);
